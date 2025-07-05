50312064a89c0dc3b457970e1242354652cbf598
dewstouh <diego@justdiego.com>
2025-07-05 22:03:55 +0200

    feat(scripts/env): add DOCKER_BASE_URL and SCRIPTS_BASE_URL

--- terraform/scripts/env/defaults.sh
+++ terraform/scripts/env/defaults.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 COOLIFY_TOKEN_PATH="/root/.coolify_api_token"
-SOURCE_BASE_URL="https://raw.githubusercontent.com/quickfra/quickfra/feature/terraform-infra/terraform/"
+SOURCE_BASE_URL="https://raw.githubusercontent.com/quickfra/quickfra/feature/terraform-infra/terraform"
+DOCKER_BASE_URL="$SOURCE_BASE_URL/docker"
+SCRIPTS_BASE_URL="$SOURCE_BASE_URL/scripts"
