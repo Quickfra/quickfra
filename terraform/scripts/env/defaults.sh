c24f8e2bd25aa912a036029827be8da0aa83d98b
dewstouh <diego@justdiego.com>
2025-07-06 10:08:37 +0200

    feat(scripts/env): add COOLIFY_DEFAULT_ENVIRONMENT variable to avoid redundancy

--- terraform/scripts/env/defaults.sh
+++ terraform/scripts/env/defaults.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-COOLIFY_TOKEN_PATH="/root/.coolify_api_token"
 DOCKER_BASE_URL="$SOURCE_BASE_URL/docker"
 SCRIPTS_BASE_URL="$SOURCE_BASE_URL/scripts"
+
+# COOLIFY
+COOLIFY_TOKEN_PATH="/root/.coolify_api_token"
+COOLIFY_DEFAULT_ENVIRONMENT="production"
