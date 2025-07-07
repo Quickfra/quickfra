6151f6c4e16e368170f64449f468ea2f7ea9e0e8
dewstouh <diego@justdiego.com>
2025-07-07 20:30:36 +0200

    refactor(scripts/lib/snappymail): use escape_sed for config replacements

--- terraform/scripts/lib/snappymail.sh
+++ terraform/scripts/lib/snappymail.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 replace_snappymail_name(){
-    local title="$APP_NAME"
+    local title=$(escape_sed "$APP_NAME")
     local config_file="$SNAPPYMAIL_CONFIG_FILE"
-    sed -i "s/SnappyMail/$title/g" "$config_file"
+    sed -i "s#SnappyMail#$title#g" "$config_file"
 }
 
 # Replace snappymail login credentials the same as the coolify panel so the user does not get confused.
@@ -11,12 +11,12 @@ replace_snappymail_name(){
 # It is recommended to change the password after the first login.
 replace_snappymail_login() {
     local config_file="$SNAPPYMAIL_CONFIG_FILE"
-    local admin_login="$COOLIFY_EMAIL"
-    local admin_password="$(hash "$COOLIFY_PASSWORD")"
+    local admin_login=$(escape_sed "$COOLIFY_EMAIL")
+    local admin_password=$(escape_sed "$(hash "$COOLIFY_PASSWORD")")
     # Replace admin login with the coolify admin email
-    sed -i "s/^admin_login = .*/admin_login = \"$admin_login\"/" "$config_file"
+    sed -i "s#^admin_login = .*#admin_login = \"$admin_login\"#" "$config_file"
     # Replace admin password with the coolify admin password
-    sed -i "s/^admin_password = .*/admin_password = \"$admin_password\"/" "$config_file"
+    sed -i "s#^admin_password = .*#admin_password = \"$admin_password\"#" "$config_file"
 }
 
 config_snappymail() {
