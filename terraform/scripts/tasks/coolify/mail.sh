4d7f0e4c0af6a92f3ef0dddf2156380678c7c2df
dewstouh <diego@justdiego.com>
2025-07-07 19:07:44 +0200

    feat(scripts/tasks/coolify/mail): config snappymail after installation

--- terraform/scripts/tasks/coolify/mail.sh
+++ terraform/scripts/tasks/coolify/mail.sh
@@ -40,6 +40,7 @@ task_setup_mail() {
   set_coolify_service_application_domain "stalwart-mail" "stalwart.$DOMAIN"
   
   local webmail_uuid="$(create_coolify_webmail_resource "$project_uuid")"
+  config_snappymail
   # TODO: Change to UUID when available
   # Warning: This is a temporary workaround until the UUID is available, as the service application UUID is not linked to the service uuid.
   # It is currently using the docker (docker/compose.yaml) service name as the ID, so be careful when changing the compose names.
