f96b7a31e7868ab5455dba5f929985e44b28a961
dewstouh <diego@justdiego.com>
2025-07-07 12:59:34 +0200

    fix(scripts/tasks/coolify): set_coolify_app_env_var log "$env_var_name" replaced to "$key" and json data "name" replaced to "key"

--- terraform/scripts/lib/coolify.sh
+++ terraform/scripts/lib/coolify.sh
@@ -58,14 +58,14 @@ set_coolify_app_env_var() {
   local key="$2"
   local value="$3"
 
-  log "[COOLIFY] :: Setting environment variable: $env_var_name for app: $app_uuid"
+  log "[COOLIFY] :: Setting environment variable: $key for app: $app_uuid"
 
   curl -s localhost:8000/api/v1/applications/$app_uuid/envs \
     --request POST \
     --header "Authorization: Bearer $(get_coolify_token)" \
     --header "Content-Type: application/json" \
     --data '{
-      "name": "'"$key"'",
+      "key": "'"$key"'",
       "value": "'"$value"'"
     }'
 }
